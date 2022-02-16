c73abb50e193e2996428f331b0f5b47b33248195
nicolas-f <github_account@nettrader.fr>
2022-02-16 17:08:25 +0100

    use the same main as 4x

--- noisemodelling-tutorial-01/src/main/java/org/noise_planet/nmtutorial01/main.java
+++ noisemodelling-tutorial-01/src/main/java/org/noise_planet/nmtutorial01/main.java
@@ -2,18 +2,28 @@ package org.noise_planet.nmtutorial01;
 
 import org.cts.crs.CRSException;
 import org.cts.op.CoordinateOperationException;
+import org.h2.value.ValueBoolean;
 import org.h2gis.api.EmptyProgressVisitor;
 import org.h2gis.api.ProgressVisitor;
 import org.h2gis.functions.io.csv.CSVDriverFunction;
 import org.h2gis.functions.io.geojson.GeoJsonRead;
+import org.h2gis.functions.io.shp.SHPWrite;
+import org.h2gis.utilities.JDBCUtilities;
 import org.h2gis.utilities.SFSUtilities;
-import org.noise_planet.noisemodelling.pathfinder.utils.*;
+import org.h2gis.utilities.TableLocation;
+import org.noise_planet.noisemodelling.jdbc.BezierContouring;
 import org.noise_planet.noisemodelling.jdbc.LDENConfig;
 import org.noise_planet.noisemodelling.jdbc.LDENPointNoiseMapFactory;
 import org.noise_planet.noisemodelling.jdbc.PointNoiseMap;
-import org.noise_planet.noisemodelling.pathfinder.FastObstructionTest;
+import org.noise_planet.noisemodelling.jdbc.TriangleNoiseMap;
 import org.noise_planet.noisemodelling.pathfinder.IComputeRaysOut;
+import org.noise_planet.noisemodelling.pathfinder.LayerDelaunayError;
 import org.noise_planet.noisemodelling.pathfinder.RootProgressVisitor;
+import org.noise_planet.noisemodelling.pathfinder.utils.JVMMemoryMetric;
+import org.noise_planet.noisemodelling.pathfinder.utils.KMLDocument;
+import org.noise_planet.noisemodelling.pathfinder.utils.ProfilerThread;
+import org.noise_planet.noisemodelling.pathfinder.utils.ProgressMetric;
+import org.noise_planet.noisemodelling.pathfinder.utils.ReceiverStatsMetric;
 import org.noise_planet.noisemodelling.propagation.ComputeRaysOutAttenuation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,10 +37,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class Main {
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException, IOException, LayerDelaunayError {
         // Init output logger
         Logger logger = LoggerFactory.getLogger(Main.class);
 
@@ -53,7 +65,7 @@ class Main {
         DateFormat df = new SimpleDateFormat("yyyyMMdd'T'HHmmss", Locale.getDefault());
 
         // Open connection to database
-        String dbName = new File(workingDir + df.format(new Date())).toURI().toString();
+        String dbName = new File(workingDir + "db_" + df.format(new Date())).toURI().toString();
         Connection connection = SFSUtilities.wrapConnection(DbUtilities.createSpatialDataBase(dbName, true));
         Statement sql = connection.createStatement();
 
@@ -68,20 +80,29 @@ class Main {
         logger.info("Import noise source");
 
         GeoJsonRead.readGeoJson(connection, Main.class.getResource("lw_roads.geojson").getFile(), "LW_ROADS");
+
         // Set primary key
         sql.execute("ALTER TABLE LW_ROADS ALTER COLUMN PK INTEGER NOT NULL");
         sql.execute("ALTER TABLE LW_ROADS ADD PRIMARY KEY (PK)");
 
         // Import BUILDINGS
 
-        logger.info("Import evaluation coordinates");
+        logger.info("Generate receivers grid for noise map rendering");
 
-        GeoJsonRead.readGeoJson(connection, Main.class.getResource("receivers.geojson").getFile(), "RECEIVERS");
-        // Set primary key
-        sql.execute("ALTER TABLE RECEIVERS ALTER COLUMN PK INTEGER NOT NULL");
-        sql.execute("ALTER TABLE RECEIVERS ADD PRIMARY KEY (PK)");
+        TriangleNoiseMap noiseMap = new TriangleNoiseMap("BUILDINGS", "LW_ROADS");
 
+        AtomicInteger pk = new AtomicInteger(0);
+        noiseMap.initialize(connection, new EmptyProgressVisitor());
+        noiseMap.setGridDim(1);
+        noiseMap.setMaximumArea(0);
+        noiseMap.setIsoSurfaceInBuildings(false);
 
+        for (int i = 0; i < noiseMap.getGridDim(); i++) {
+            for (int j = 0; j < noiseMap.getGridDim(); j++) {
+                logger.info("Compute cell " + (i * noiseMap.getGridDim() + j + 1) + " of " + noiseMap.getGridDim() * noiseMap.getGridDim());
+                noiseMap.generateReceivers(connection, i, j, "RECEIVERS", "TRIANGLES", pk);
+            }
+        }
         // Import MNT
 
         logger.info("Import digital elevation model");
@@ -91,18 +112,14 @@ class Main {
         // Init NoiseModelling
         PointNoiseMap pointNoiseMap = new PointNoiseMap("BUILDINGS", "LW_ROADS", "RECEIVERS");
 
-        pointNoiseMap.setMaximumPropagationDistance(160.0d);
-        pointNoiseMap.setSoundReflectionOrder(0);
-        pointNoiseMap.setComputeHorizontalDiffraction(true);
+        pointNoiseMap.setMaximumPropagationDistance(800.0);
+        pointNoiseMap.setSoundReflectionOrder(1);
+        pointNoiseMap.setComputeHorizontalDiffraction(false);
         pointNoiseMap.setComputeVerticalDiffraction(true);
         // Building height field name
         pointNoiseMap.setHeightField("HEIGHT");
         // Point cloud height above sea level POINT(X Y Z)
         pointNoiseMap.setDemTable("DEM");
-        // Do not propagate for low emission or far away sources.
-        // error in dB
-        pointNoiseMap.setMaximumError(0.1d);
-        pointNoiseMap.setNoiseFloor(35d);
 
         // Init custom input in order to compute more than just attenuation
         // LW_ROADS contain Day Evening Night emission spectrum
@@ -124,10 +141,16 @@ class Main {
 
         pointNoiseMap.initialize(connection, new EmptyProgressVisitor());
 
-        // force the creation of a 2x2 cells
-        pointNoiseMap.setGridDim(2);
-
-
+        LocalDateTime now = LocalDateTime.now();
+        ProfilerThread profilerThread = new ProfilerThread(new File(String.format("profile_%d_%d_%d_%dh%d.csv",
+                now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now.getHour(), now.getMinute())));
+        profilerThread.addMetric(tableWriter);
+        profilerThread.addMetric(new ProgressMetric(progressLogger));
+        profilerThread.addMetric(new JVMMemoryMetric());
+        profilerThread.addMetric(new ReceiverStatsMetric());
+        profilerThread.setWriteInterval(60);
+        profilerThread.setFlushInterval(60);
+        pointNoiseMap.setProfilerThread(profilerThread);
         // Set of already processed receivers
         Set<Long> receivers = new HashSet<>();
 
@@ -135,14 +158,6 @@ class Main {
         long start = System.currentTimeMillis();
 
         // Iterate over computation areas
-        ProfilerThread profilerThread = new ProfilerThread(new File("target/profile.csv"));
-        profilerThread.addMetric(tableWriter);
-        profilerThread.addMetric(new ProgressMetric(progressLogger));
-        profilerThread.addMetric(new JVMMemoryMetric());
-        profilerThread.addMetric(new ReceiverStatsMetric());
-        profilerThread.setWriteInterval(2);
-        profilerThread.setFlushInterval(15);
-        pointNoiseMap.setProfilerThread(profilerThread);
         try {
             tableWriter.start();
             new Thread(profilerThread).start();
@@ -152,47 +167,30 @@ class Main {
             for(PointNoiseMap.CellIndex cellIndex : new TreeSet<>(cells.keySet())) {
                 // Run ray propagation
                 IComputeRaysOut out = pointNoiseMap.evaluateCell(connection, cellIndex.getLatitudeIndex(), cellIndex.getLongitudeIndex(), progressVisitor, receivers);
-                // Export as a Google Earth 3d scene
-                if (out instanceof ComputeRaysOutAttenuation) {
-                    ComputeRaysOutAttenuation cellStorage = (ComputeRaysOutAttenuation) out;
-                    exportScene(String.format(Locale.ROOT,"target/scene_%d_%d.kml", cellIndex.getLatitudeIndex(), cellIndex.getLongitudeIndex()), cellStorage.inputData.freeFieldFinder, cellStorage);
-                }
             }
         } finally {
             profilerThread.stop();
             tableWriter.stop();
         }
-
         long computationTime = System.currentTimeMillis() - start;
         logger.info(String.format(Locale.ROOT, "Computed in %d ms, %.2f ms per receiver", computationTime,computationTime / (double)receivers.size()));
-        // Export result tables as csv files
-        CSVDriverFunction csv = new CSVDriverFunction();
-        csv.exportTable(connection, ldenConfig.getlDayTable(), new File("target/"+ldenConfig.getlDayTable()+".csv"), new EmptyProgressVisitor());
-        csv.exportTable(connection, ldenConfig.getlEveningTable(), new File("target/"+ldenConfig.getlEveningTable()+".csv"), new EmptyProgressVisitor());
-        csv.exportTable(connection, ldenConfig.getlNightTable(), new File("target/"+ldenConfig.getlNightTable()+".csv"), new EmptyProgressVisitor());
-        csv.exportTable(connection, ldenConfig.getlDenTable(), new File("target/"+ldenConfig.getlDenTable()+".csv"), new EmptyProgressVisitor());
 
+
+        logger.info("Create iso contours");
+        int srid = SFSUtilities.getSRID(connection, TableLocation.parse("LW_ROADS"));
+        List<Double> isoLevels = BezierContouring.NF31_133_ISO; // default values
+        sql.execute("ALTER TABLE " + ldenConfig.getlDenTable() +
+                " ADD COLUMN THE_GEOM geometry");
+        sql.execute(" UPDATE "+ldenConfig.getlDenTable()+" SET THE_GEOM = (SELECT THE_GEOM FROM RECEIVERS R " +
+                "WHERE R.PK = " + ldenConfig.getlDenTable() + ".IDRECEIVER)");
+        BezierContouring bezierContouring = new BezierContouring(isoLevels, srid);
+        bezierContouring.setSmoothCoefficient(0.5);
+        bezierContouring.setPointTable(ldenConfig.getlDenTable());
+        bezierContouring.createTable(connection);
+        logger.info("Export iso contours");
+        SHPWrite.exportTable(connection, "target/"+bezierContouring.getOutputTable()+".shp", bezierContouring.getOutputTable());
     }
 
 
-    public static void exportScene(String name, FastObstructionTest manager, ComputeRaysOutAttenuation result) throws IOException {
-        try {
-            FileOutputStream outData = new FileOutputStream(name);
-            KMLDocument kmlDocument = new KMLDocument(outData);
-            kmlDocument.setInputCRS("EPSG:2154");
-            kmlDocument.writeHeader();
-            if(manager != null) {
-                kmlDocument.writeTopographic(manager.getTriangles(), manager.getVertices());
-            }
-            if(result != null) {
-                kmlDocument.writeRays(result.getPropagationPaths());
-            }
-            if(manager != null && manager.isHasBuildingWithHeight()) {
-                kmlDocument.writeBuildings(manager);
-            }
-            kmlDocument.writeFooter();
-        } catch (XMLStreamException | CoordinateOperationException | CRSException ex) {
-            throw new IOException(ex);
-        }
-    }
 }
+
