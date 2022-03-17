4abee28bad78549843d791c24794b6931abc9fe3
nicolas-f <github_account@nettrader.fr>
2022-03-17 16:54:08 +0100

    push another unit test

--- noisemodelling-propagation/src/test/java/org/noise_planet/noisemodelling/propagation/RayAttenuationTest.java
+++ noisemodelling-propagation/src/test/java/org/noise_planet/noisemodelling/propagation/RayAttenuationTest.java
@@ -3,10 +3,14 @@ package org.noise_planet.noisemodelling.propagation;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.Test;
+import org.locationtech.jts.geom.Coordinate;
 import org.noise_planet.noisemodelling.pathfinder.PointPath;
 import org.noise_planet.noisemodelling.pathfinder.PropagationPath;
+import org.noise_planet.noisemodelling.pathfinder.SegmentPath;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 
@@ -26,6 +30,25 @@ public class RayAttenuationTest {
                 .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
         PropagationPath path = mapper.readValue(
                 RayAttenuationTest.class.getResourceAsStream("special_ray.json"), PropagationPath.class);
+        PropagationProcessPathData propagationProcessPathData = new PropagationProcessPathData(false);
+        double[] aBoundary = EvaluateAttenuationCnossos.aBoundary(path, propagationProcessPathData);
+        for(double value : aBoundary) {
+            assertFalse(Double.isNaN(value));
+        }
+    }
+
+    @Test
+    public void testPropagationPathReceiverUnder2() throws IOException {
+        JsonMapper.Builder builder = JsonMapper.builder();
+        JsonMapper mapper = builder.build();
+        mapper.setVisibility(mapper.getSerializationConfig().getDefaultVisibilityChecker()
+                .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+                .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+        PropagationPath path = mapper.readValue(
+                RayAttenuationTest.class.getResourceAsStream("special_ray2.json"), PropagationPath.class);
         PropagationProcessPathData propagationProcessPathData = new PropagationProcessPathData(false);
         double[] aBoundary = EvaluateAttenuationCnossos.aBoundary(path, propagationProcessPathData);
         for(double value : aBoundary) {
